e8afbef690c500715edd0ac7a37e78e46fac87b9
JamesEwoenamAdogo <jamesewoenam7@gmail.com>
2024-09-07 16:39:03 +0000

    Update server.js

--- backend/api/server.js
+++ backend/api/server.js
@@ -11,7 +11,9 @@ dotenv.config()
 
 const app= express();
 app.use(express.json());
-app.use(cors({}));
+app.use(cors({
+    origin:"https://food-ordering-two-chi.vercel.app"
+}));
 app.use("/images", express.static("uploads"))
 
 const PORT = process.env.PORT
